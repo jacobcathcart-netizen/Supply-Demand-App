1914264ca965ddac2d22d352eac51a59df585d03
Jacob.Cathcart <jacob.cathcart@ccrenew.com>
2026-03-19 13:57:01 -0400

    homepage update

--- Home.py
+++ Home.py
@@ -13,7 +13,6 @@ def get_snowflake_connection():
         authenticator=st.secrets["snowflake"].get("authenticator", "externalbrowser"),
         warehouse=st.secrets["snowflake"]["warehouse"],
         database=st.secrets["snowflake"]["database"],
-        schema=st.secrets["snowflake"]["schema"],
         role=st.secrets["snowflake"]["role"],
     )
 
