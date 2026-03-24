f6579af542a8bd038f1810a93fababae6a1f6f04
Jacob.Cathcart <jacob.cathcart@ccrenew.com>
2026-03-24 16:01:01 -0400

    ffff

--- components/visuals.py
+++ components/visuals.py
@@ -446,8 +446,8 @@ def supply_delta_chart(
     ax1.legend(
         handles1 + handles2 + handles3,
         labels1 + labels2 + labels3,
-        loc=2,
-        bbox_to_anchor=(-.3, 1),
+        loc=7,
+        bbox_to_anchor=(1.02, 0),
         ncol=1,
     )
 
